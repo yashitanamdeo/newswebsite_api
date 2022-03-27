1c2fbd8048776b9994a876ec83bc41c300d792bb
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-27 18:41:36 +0530

    added route for technews

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnewsComponent } from './technews/technews.component';
 import { TopheadingComponent } from './topheading/topheading.component';
 
 const routes: Routes = [
   {path: '', component:TopheadingComponent}, //home
+  {path: 'tech', component:TechnewsComponent}, //tech news
 ];
 
 @NgModule({
