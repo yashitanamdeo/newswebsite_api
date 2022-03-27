164bec0cdceed4fc3d3287aafdff2d492ed5ccd6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-27 19:49:17 +0530

    business news html + css + routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessnewsComponent } from './businessnews/businessnews.component';
 import { TechnewsComponent } from './technews/technews.component';
 import { TopheadingComponent } from './topheading/topheading.component';
 
 const routes: Routes = [
   {path: '', component:TopheadingComponent}, //home
   {path: 'tech', component:TechnewsComponent}, //tech news
+  {path: 'business', component:BusinessnewsComponent}, //business
 ];
 
 @NgModule({
