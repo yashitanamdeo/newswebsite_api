e790de2ce2a9768ae8075a5a6bc0b77fa4183886
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-27 19:31:41 +0530

    formatted document

--- src/app/topheading/topheading.component.ts
+++ src/app/topheading/topheading.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {NewsapiservicesService} from '../service/newsapiservices.service';
+import { NewsapiservicesService } from '../service/newsapiservices.service';
 
 @Component({
   selector: 'app-topheading',
   templateUrl: './topheading.component.html',
-  styleUrls: ['./topheading.component.css']
+  styleUrls: ['./topheading.component.css'],
 })
 export class TopheadingComponent implements OnInit {
-
-  constructor(private _services:NewsapiservicesService) { }
+  constructor(private _services: NewsapiservicesService) {}
 
   // display data
   topheadingDisplay: any = [];
 
   ngOnInit(): void {
-
     this._services.topHeading().subscribe((result) => {
       console.log(result);
       this.topheadingDisplay = result.articles;
-
-    })
+    });
   }
-
 }
