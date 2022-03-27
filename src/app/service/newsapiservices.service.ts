2f551579fe8ffeee4e9a179432cc8b28387c30c7
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-27 17:17:33 +0530

    updated country to 'us'

--- src/app/service/newsapiservices.service.ts
+++ src/app/service/newsapiservices.service.ts
@@ -9,7 +9,7 @@ export class NewsapiservicesService {
 
   constructor(private _http: HttpClient) { }
   // newsApiUrl
-  newsApiUrl = "https://newsapi.org/v2/top-headlines?country=ind&apiKey=0330aeb0b55c4e899e6f6655108aa339"
+  newsApiUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0330aeb0b55c4e899e6f6655108aa339"
 
   //topheading()
   topHeading():Observable<any>
